c7188333521f89f3352dfa23810c95c4f5d78b07
vekotov <vekotov.24@yandex.ru>
2021-04-26 19:13:24 +0700

    Add errors, remove unused variables

--- src/main/java/ru/vekotov/microtwitter/GreetingController.java
+++ src/main/java/ru/vekotov/microtwitter/GreetingController.java
@@ -15,8 +15,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
-import static ru.vekotov.microtwitter.MicrotwitterApplication.*;
-
 @Controller
 @EnableMongoRepositories
 @EntityScan
@@ -32,9 +30,11 @@ public class GreetingController {
 
     @GetMapping("/")
     public String index(@CookieValue(value = "token", defaultValue = "null") String token,
+                        @RequestParam(name="err", required = false, defaultValue = "no") String error,
                         Model model) {
         model.addAttribute("postList", postRepository.findAll());
         Token userToken = tokenRepository.findTokenByToken(token);
+        model.addAttribute("err", error);
         if(userToken != null) model.addAttribute("status", "yes_login");
         else model.addAttribute("status","no_login");
         return "index";
@@ -42,13 +42,14 @@ public class GreetingController {
 
     @PostMapping("/create_post")
     public String createPost(
-            @RequestParam(name="text") String text,
+            @RequestParam(name = "text") String text,
             @CookieValue(value = "token", defaultValue = "null") String token) {
+        if(text.length() > 500) return "redirect:/?err=too_long_message";
         if(text
                 .replace(" ", "")
                 .replace("\t", "")
                 .replace("\n", "")
-                .equals("")) return "redirect:/";
+                .equals("")) return "redirect:/?err=text_empty";
         Token userToken = tokenRepository.findTokenByToken(token);
         if(userToken == null) {
             postRepository.save(new Post("Anonymous", text));
@@ -60,32 +61,46 @@ public class GreetingController {
 
     @GetMapping("/login")
     public String login(@CookieValue(value = "token", defaultValue = "null") String token,
+                        @RequestParam(name="err", required = false, defaultValue = "no") String error,
                         Model model) {
         Token userToken = tokenRepository.findTokenByToken(token);
-        if(userToken != null) return "redirect:/";
-        model.addAttribute("status","no_login");
+        if(userToken != null) return "redirect:/?err=already_login";
+        model.addAttribute("err", error);
+
         return "login";
     }
 
     @PostMapping("/login")
     public String postLogin(
-            @RequestParam(name="login") String login,
-            @RequestParam(name="password") String password,
+            @RequestParam(name = "login") String login,
+            @RequestParam(name = "password") String password,
             HttpServletResponse response) {
-        if(checkPassword(login, password)) return "redirect:/login";
+        if(login.length() == 0 || password.length() == 0) return "redirect:/login?err=login_password_or_login_empty";
+        if(userRepository.findUserByLogin(login) == null) return "redirect:/login?err=login_no_such_user";
+        if(!checkPassword(login, password)) return "redirect:/login?err=login_wrong_password";
+
         addToken(login, response);
         return "redirect:/";
     }
 
     @PostMapping("/register")
     public String postRegister(
-            @RequestParam(name="login", required=true) String login,
-            @RequestParam(name="password", required=true) String password,
-            @RequestParam(name="password_again", required = true) String passwordAgain,
+            @RequestParam(name="login") String login,
+            @RequestParam(name="password") String password,
+            @RequestParam(name="password_again") String passwordAgain,
+            Model model,
             HttpServletResponse response) {
-        if(!password.equals(passwordAgain)) return "redirect:/login";
+        model.addAttribute("login_result", "null");
+        if(login.length() == 0 || password.length() == 0) {
+            return "redirect:/login?err=register_password_or_login_empty";
+        }
+        if(!password.equals(passwordAgain)) {
+            return "redirect:/login?err=register_passwords_not_equal";
+        }
         User findUser = userRepository.findUserByLogin(login);
-        if(findUser != null) return "redirect:/login";
+        if(findUser != null) {
+            return "redirect:/login?err=register_user_already_exist";
+        }
         User user = new User(login, hashPassword(password));
         userRepository.save(user);
         addToken(login, response);
--- src/main/java/ru/vekotov/microtwitter/MicrotwitterApplication.java
+++ src/main/java/ru/vekotov/microtwitter/MicrotwitterApplication.java
@@ -1,34 +1,19 @@
 package ru.vekotov.microtwitter;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import java.util.*;
+import java.util.Collections;
 
 @SpringBootApplication
 public class MicrotwitterApplication {
-	static Logger logger = LoggerFactory.getLogger(MicrotwitterApplication.class);
-
-	public static Deque<Post> postList = new ArrayDeque<>();
-	public static Map<String, User> userList = new HashMap<>();
-	public static Map<String, User> tokens = new HashMap<>();
-
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(MicrotwitterApplication.class);
 		app.setDefaultProperties(Collections
@@ -42,9 +27,7 @@ public class MicrotwitterApplication {
 		MappingMongoConverter converter = new MappingMongoConverter(new DefaultDbRefResolver(mongoDbFactory), context);
 		converter.setTypeMapper(new DefaultMongoTypeMapper(null));
 
-		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory, converter);
-
-		return mongoTemplate;
+		return new MongoTemplate(mongoDbFactory, converter);
 
 	}
 }
